134032417a0573814bd4fd23c9b9743832e69b08
Kamil Trzcinski <ayufan@ayufan.eu>
2017-11-30 10:28:49 +0100

    Remove rails dependency on merge-yaml

--- dockerfiles/scripts/helpers/merge-yaml.rb
+++ dockerfiles/scripts/helpers/merge-yaml.rb
@@ -1,10 +1,31 @@
 #!/usr/bin/ruby
 
 require 'yaml'
-require 'rails'
+
+def deep_merge!(hash, other_hash, &block)
+  other_hash.each_pair do |current_key, other_value|
+    this_value = hash[current_key]
+
+    hash[current_key] = if this_value.is_a?(Hash) && other_value.is_a?(Hash)
+      deep_merge(this_value, other_value, &block)
+    else
+      if block_given? && key?(current_key)
+        block.call(current_key, this_value, other_value)
+      else
+        other_value
+      end
+    end
+  end
+
+  hash
+end
+
+def deep_merge(hash, other_hash, &block)
+  deep_merge!(hash.dup, other_hash, &block)
+end
 
 config = ARGV.inject({}) do |config, arg|
-  config.deep_merge(YAML.load_file(arg) || {})
+  deep_merge(config, YAML.load_file(arg) || {})
 end
 
 puts config.to_yaml
