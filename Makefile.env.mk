6e218f210e144cd3bfd232d704b5a32edccc0ebc
Kamil Trzcinski <ayufan@ayufan.eu>
2017-12-01 11:18:02 +0100

    Don't load application settings when provisioning

--- Makefile.env.mk
+++ Makefile.env.mk
@@ -1,25 +1,30 @@
 .PHONY: create-dev
 create-dev: deps
-	./scripts/proxy docker-compose run -e RAILS_ENV=development spring bash -c 'bin/rake db:create && bin/rake dev:setup'
+	./scripts/proxy docker-compose run -e RAILS_ENV=development -e IN_MEMORY_APPLICATION_SETTINGS=true \
+		spring bash -c 'bin/rake db:create && bin/rake dev:setup'
 
 .PHONY: create-test
 create-test: deps
-	./scripts/proxy docker-compose run -e RAILS_ENV=test spring bash -c 'bin/rake db:drop; bin/rake db:create && bin/rake db:setup'
+	./scripts/proxy docker-compose run -e RAILS_ENV=test -e IN_MEMORY_APPLICATION_SETTINGS=true \
+		spring bash -c 'bin/rake db:drop; bin/rake db:create && bin/rake db:setup'
 
 .PHONY: create-runner
 create-runner: deps
-	./scripts/proxy docker-compose run -e RAILS_ENV=development spring bin/rails runner "Ci::Runner.create(is_shared: true, token: 'SHARED_RUNNER_TOKEN')"
+	./scripts/proxy docker-compose run -e RAILS_ENV=development -e IN_MEMORY_APPLICATION_SETTINGS=true \
+		spring bin/rails runner "Ci::Runner.create(is_shared: true, token: 'SHARED_RUNNER_TOKEN')"
 
 .PHONY: create
 create: create-dev create-test create-runner
 
 .PHONY: migrate-dev
 migrate-dev:
-	./scripts/proxy docker-compose run -e RAILS_ENV=development spring bash -c 'bin/rake db:migrate'
+	./scripts/proxy docker-compose run -e RAILS_ENV=development -e IN_MEMORY_APPLICATION_SETTINGS=true \
+		spring bash -c 'bin/rake db:migrate'
 
 .PHONY: update-test
 migrate-test:
-	./scripts/proxy docker-compose run -e RAILS_ENV=test spring bash -c 'bin/rake db:migrate'
+	./scripts/proxy docker-compose run -e RAILS_ENV=test -e IN_MEMORY_APPLICATION_SETTINGS=true \
+		spring bash -c 'bin/rake db:migrate'
 
 .PHONY: update-dev
 update-dev: update-repos
