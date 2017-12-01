85c6f6eceff6809dee46ef56fec564973dbcaca1
Kamil Trzcinski <ayufan@ayufan.eu>
2017-12-01 11:12:48 +0100

    Use sudo for sshd

--- dockerfiles/scripts/entrypoint/gitlab-rails-env.sh
+++ dockerfiles/scripts/entrypoint/gitlab-rails-env.sh
@@ -4,18 +4,13 @@ set -xe
 
 export BUNDLE_JOBS=$(nproc)
 
-DROP_PRIVILEGES=
-if [[ $(id -u) -eq 0 ]]; then
-    DROP_PRIVILEGES="su git -c"
+if ! gem list -i bundler; then
+    gem install bundler
 fi
 
-if ! $DROP_PRIVILEGES gem list -i bundler; then
-    $DROP_PRIVILEGES gem install bundler
-fi
-
-$DROP_PRIVILEGES /scripts/helpers/fix-perms.sh
-$DROP_PRIVILEGES /scripts/helpers/configure-gitlab-shell.sh
-$DROP_PRIVILEGES /scripts/helpers/configure-gitlab-rails.sh
-$DROP_PRIVILEGES /scripts/helpers/install-gettext.sh
+/scripts/helpers/fix-perms.sh
+/scripts/helpers/configure-gitlab-shell.sh
+/scripts/helpers/configure-gitlab-rails.sh
+/scripts/helpers/install-gettext.sh
 
 exec "$@"
