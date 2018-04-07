f150ee64e842f6f1243356bd63fcde2f095e8247
Kamil Trzciński <ayufan@ayufan.eu>
2018-04-07 09:38:35 +0200

    Require sudo access on osx

--- Makefile.sync.mk
+++ Makefile.sync.mk
@@ -1,3 +1,8 @@
 sync: repos
 	@bash -c 'source .env && ./scripts/ssh mkdir -p "$$SSH_TARGET_DIR/data" && echo OK'
+ifneq (,$(wildcard /dev/fsevents))
+	@echo "lsyncd on OSX requires sudo access"
+	sudo ./scripts/env lsyncd lsyncd.lua
+else
 	./scripts/env lsyncd lsyncd.lua
+endif
