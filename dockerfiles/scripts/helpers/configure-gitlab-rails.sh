fe6426560420d11d34decdc30f33a4125b467854
Kamil Trzciński <ayufan@ayufan.eu>
2018-04-25 13:11:10 +0200

    Revert "Use webpack dev server for testing"
    This reverts commit f681c7e04fad9e29e76aa81709bb43c0a8ecd8c9.

--- dockerfiles/scripts/helpers/configure-gitlab-rails.sh
+++ dockerfiles/scripts/helpers/configure-gitlab-rails.sh
@@ -65,12 +65,6 @@ development:
       port: 3808
   uploads:
     storage_path: /home/git/gitlab/public/
-test:
-  webpack:
-    dev_server:
-      enabled: true
-      host: webpack
-      port: 3808
 EOF
 
 if [[ ! -e /home/git/registry-auth.crt ]]; then
