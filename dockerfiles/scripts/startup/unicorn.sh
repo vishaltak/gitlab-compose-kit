48320ec32d9cd9ac4cb28a9dd3e4cbc61e77c4f2
Kamil Trzciński <ayufan@ayufan.eu>
2018-05-28 13:57:41 +0200

    Fix rails/server.pid problem

--- dockerfiles/scripts/startup/unicorn.sh
+++ dockerfiles/scripts/startup/unicorn.sh
@@ -3,7 +3,8 @@
 cd /home/git/gitlab
 
 if [[ -n "$USE_RAILS_SERVER" ]]; then
-    exec bundle exec rails server -b "0.0.0.0" -p 8080 -e "$RAILS_ENV"
+    rm -f /tmp/rails-server.pid
+    exec bundle exec rails server -b "0.0.0.0" -p 8080 -e "$RAILS_ENV" -P /tmp/rails-server.pid
 else
     exec bundle exec unicorn -E "$RAILS_ENV" -c config/unicorn.rb
 fi
