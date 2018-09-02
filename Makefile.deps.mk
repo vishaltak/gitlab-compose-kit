74aa04171158b7ea06e58d54fdb9c85265cce060
Kamil Trzciński <ayufan@ayufan.eu>
2018-09-02 16:16:54 +0200

    Allow to push build image

--- Makefile.deps.mk
+++ Makefile.deps.mk
@@ -2,3 +2,9 @@
 build:
 	@echo Building environment...
 	@./scripts/proxy docker-compose build
+
+.PHONY: push
+push: build
+	@echo Checking version...
+	@git describe --exact-match
+	docker-compose push
