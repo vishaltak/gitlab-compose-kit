024fb774d80ad4c9643f26bc2847f5ca2181e059
Kamil Trzciński <ayufan@ayufan.eu>
2018-10-19 17:00:09 +0200

    Optimise webpack to use persistent volume cache

--- dockerfiles/scripts/startup/webpack.sh
+++ dockerfiles/scripts/startup/webpack.sh
@@ -5,8 +5,17 @@ set -xe
 /scripts/helpers/fix-perms.sh
 
 cd /home/git/gitlab
+
 mkdir -p /data/cache/node_modules
 ln -sf /data/cache/node_modules || true
+
+# migrate old path
+if [[ -d tmp/cache ]]; then
+  rm -rf tmp/cache
+fi
+
+mkdir -p /data/cache/gitlab/cache
+ln -sf /data/cache/gitlab/cache tmp/
 
 yarn install
 exec yarn dev-server
