e76b88b8d96bbaf432e05b7a65a0c3c0cafced84
Matthias Kaeppler <mkaeppler@gitlab.com>
2022-08-12 10:15:53 +0200

    Call fix-perms before configuring bundler

--- dockerfiles/scripts/entrypoint/gitlab-shell-env.sh
+++ dockerfiles/scripts/entrypoint/gitlab-shell-env.sh
@@ -6,11 +6,12 @@ if ! gem list -i bundler; then
     gem install bundler
 fi
 
+/scripts/helpers/fix-perms.sh
+
 source /scripts/helpers/configure-gitlab-tracing.sh
 source /scripts/helpers/configure-bundle-config.sh
 source /scripts/helpers/configure-gitaly-version-fix.sh
 
-/scripts/helpers/fix-perms.sh
 /scripts/helpers/configure-gitlab-shell.sh
 
 exec "$@"
