9e6f5987bc18f37fb3396fa4e60c0d848993a37e
Kamil Trzciński <ayufan@ayufan.eu>
2023-01-30 15:06:02 +0100

    The spring container monitors if it is still used by `exec`
    Since all processes run by `docker-compose run` or `exec`
    are parent of `-ppid 0` we can use it to count how many
    shells are in use.
    
    If there are no shells (except `run`) exit after some
    grace period.

--- dockerfiles/scripts/startup/spring.sh
+++ dockerfiles/scripts/startup/spring.sh
@@ -7,4 +7,30 @@ _term() {
 
 trap _term SIGTERM
 
-cat
+is_used() {
+  # The ppid 0 are processes created by `run` or `exec` of `docker-compose`
+  # The `run` uses 1 process, this is why it has to be `-gt 1`
+  local cnt=$(pgrep -P 0 | wc -l)
+  echo "Used by $cnt processes..."
+  [[ $cnt -gt 1 ]]
+}
+
+# Call the given method until it succeeds
+retry() {
+  for i in 1 2 3 4 5 6 7 8 9 10; do
+    if eval "$@"; then
+      return 0
+    fi
+
+    sleep 5s
+    echo "Retrying $i..."
+  done
+
+  return 1
+}
+
+while retry is_used; do
+  sleep 60s
+done
+
+echo 'No more `docker-compose exec` running. Exiting'
