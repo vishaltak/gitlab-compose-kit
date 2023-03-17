851290714aad5008afc812577caaf612423f840b
Kamil Trzciński <ayufan@ayufan.eu>
2023-03-17 10:51:02 +0100

    gitlab-tmp: fix existing directories

--- dockerfiles/scripts/helpers/fix-gitlab-tmp.sh
+++ dockerfiles/scripts/helpers/fix-gitlab-tmp.sh
@@ -7,6 +7,11 @@ CACHE_DIR=/data/cache/gitlab
 for cached in node_modules tmp/cache tmp/tests; do
   [[ -L "$cached" ]] && [[ -e "$cached" ]] && continue
 
+  if [[ -d "$cached" ]]; then
+    echo ">> The '$PWD/${cached}' already exists. This might lead to unoptimal performance. Remove to fix it."
+    continue
+  fi
+
   echo ">> Ensuring that '$PWD/${cached}' is cached..."
   name=$(basename "$cached")
 
