662e6f1ec6f30aa4279a168e1c7a0da1d8f18fca
Kamil Trzciński <ayufan@ayufan.eu>
2023-06-26 09:32:43 +0200

    Allow to use Gitaly Praefect
    Enable with `ENABLE_PRAEFECT=1` in `gck.env`.

--- dockerfiles/scripts/startup/gitaly.sh
+++ dockerfiles/scripts/startup/gitaly.sh
@@ -23,27 +23,43 @@ if [[ -n "$FORCE_GITALY_COMPILE" ]]; then
   exit 0
 fi
 
+REPO_PATH="/data/repositories"
+CONFIG_PATH="/tmp/gitaly-config.toml"
+GITALY_PORT="10000"
+PROMETHEUS_PORT="11000"
+STORAGE_NAME="default"
+
+if [[ -n "$1" ]]; then
+  REPO_PATH="$REPO_PATH/@gitaly-$1"
+  GITALY_PORT=$(($GITALY_PORT+$1))
+  PROMETHEUS_PORT=$(($PROMETHEUS_PORT+$1))
+  STORAGE_NAME="praefect-git-$1"
+  CONFIG_PATH="/tmp/gitaly-config-$1.toml"
+fi
+
+mkdir -p "$REPO_PATH"
+
 # Remove: `gitlab_url=`
 # - some older gitaly versions do break as they require `url`
 # - will be fixed with https://gitlab.com/gitlab-org/gitaly/-/merge_requests/2240
-
 # TODO: merge toml files
 sed \
   -e 's|^socket_path|# socket_path|' \
-  -e 's|^# prometheus_listen_addr.*|prometheus_listen_addr = "0.0.0.0:9236"|' \
-  -e 's|^# listen_addr.*|listen_addr = "0.0.0.0:9999"|' \
-  -e 's|^path .*|path = "/data/repositories"|' \
+  -e "s|^# prometheus_listen_addr.*|prometheus_listen_addr = \"0.0.0.0:$PROMETHEUS_PORT\"|" \
+  -e "s|^# listen_addr.*|listen_addr = \"0.0.0.0:$GITALY_PORT\"|" \
+  -e "s|^path .*|path = \"$REPO_PATH\"|" \
+  -e "s|^name .*|name = \"$STORAGE_NAME\"|" \
   -e 's|^url .*|url = "http://workhorse:8181"|' \
   -e 's|^secret_file .*|secret_file = "/scripts/templates/gitlab-shell-secret"|' \
   -e 's|^gitlab_url .*|url = "http://workhorse:8181"|' \
   -e 's|^# \[git\]|[git]|' \
   -e 's|^# bin_path .*|use_bundled_binaries = true|' \
-  config.toml.example | sponge /tmp/gitaly-config.toml
+  config.toml.example | sponge "$CONFIG_PATH"
 
 # Gitaly does not install into top-level dir anymore
 # https://gitlab.com/gitlab-org/gitaly/-/commit/eb6fd60561cffdbb183e74456268439bad60b21c
 if [[ -e _build/bin/gitaly ]]; then
-  exec ./_build/bin/gitaly /tmp/gitaly-config.toml
+  exec ./_build/bin/gitaly "$CONFIG_PATH"
 else
-  exec ./gitaly /tmp/gitaly-config.toml
+  exec ./gitaly "$CONFIG_PATH"
 fi
---
+++ dockerfiles/scripts/startup/praefect.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -xeo pipefail
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/.."
+
+if [[ -e ruby/Gemfile ]]; then
+  source $SCRIPT_DIR/helpers/configure-jemalloc2.sh
+
+  pushd ruby
+  if ! bundle install --quiet --local; then
+    bundle install
+  fi
+  popd
+
+  # ensure that we do not re-install all dependencies
+  export BUNDLE_FLAGS=--local
+fi
+
+cd /home/git/gitaly
+make WITH_BUNDLED_GIT=YesPlease
+
+if [[ -n "$FORCE_GITALY_COMPILE" ]]; then
+  echo "Forced gitaly compile!"
+  exit 0
+fi
+
+$SCRIPT_DIR/helpers/wait-for-service.sh tcp postgres 5432
+
+cat <<EOF | PGHOST=postgres PGUSER=postgres PGPASSWORD=password psql
+  SELECT 'CREATE DATABASE gitlab_prafect'
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'gitlab_prafect')\gexec
+EOF
+
+_build/bin/praefect -config $SCRIPT_DIR/templates/gitaly-config.praefect.toml sql-migrate
+
+$SCRIPT_DIR/startup/gitaly.sh 100 |& ts "gitaly-100: " &
+$SCRIPT_DIR/startup/gitaly.sh 101 |& ts "gitaly-101: " &
+$SCRIPT_DIR/startup/gitaly.sh 102 |& ts "gitaly-102: " &
+
+_build/bin/praefect -config $SCRIPT_DIR/templates/gitaly-config.praefect.toml serve |& ts "praefect:   "
